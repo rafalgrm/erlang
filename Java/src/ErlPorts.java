9ad54c4db95b2722a8b93f9c1fbb62046c0a3c6c
lpetka <lukasz_petka@interia.pl>
2015-01-28 22:31:50 +0100

    works

--- Java/src/ErlPorts.java
+++ Java/src/ErlPorts.java
@@ -1,25 +1,24 @@
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+
 public class ErlPorts {
-    private static int readLine(byte[] bytes) {
-        try {
+    private static int readLine(byte[] bytes) throws IOException{
             int len;
             if (readExact(bytes, 2) != 2)
                 return -1;
             len = (bytes[0] << 8) | bytes[1];
             return readExact(bytes, len);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return -1;
     }
 
-    private static int writeLine(byte[] bytes, int length) {
-        byte[] li;
-        li = ByteBuffer.allocate(4).putInt((length >> 8) & 0xff).array();
-        writeExact(li, 1);
-        li = ByteBuffer.allocate(4).putInt(length & 0xff).array();
-        writeExact(li, 1);
+    private static int writeLine(byte[] bytes, int length) throws UnsupportedEncodingException{
+        char[] li;
+        li = Character.toChars((length >> 8) & 0xff);
+        writeExact(new String(li).getBytes("UTF-8"), 1);
+        li = Character.toChars(length & 0xff);
+        writeExact(new String(li).getBytes("UTF-8"), 1);
         return writeExact(bytes, length);
     }
 
@@ -38,15 +37,15 @@ public class ErlPorts {
     private static int writeExact(byte[] buf, int len)
     {
         int wrote = 0;
+
         do {
-            System.out.write(buf,wrote,len-wrote);
-            if (len-wrote <= 0)
-                return (len-wrote);
-            wrote += len-wrote;
+            System.out.write(buf, wrote, len - wrote);
+
+            wrote += 2;
         } while (wrote<len);
+
         return (len);
     }
-
     private static int foo(int arg) {
         return arg*2;
     }
@@ -54,23 +53,21 @@ public class ErlPorts {
         return arg/2;
     }
     public static void main(String[] args) throws IOException {
-       /* int fn, arg, res = -1;
-        byte[] buf = new byte[100];*/
-        /*while (readLine(buf) > 0) {
+        int fn, arg, res = -1;
+        byte[] buf = new byte[100];
+
+        while (readLine(buf) > 0) {
             fn = buf[0];
             arg = buf[1];
-            System.out.print(fn);
+
             if (fn == 1) {
                 res = foo(arg);
             } else if (fn == 2) {
                 res = bar(arg);
             }
-            buf[0] = ByteBuffer.allocate(4).putInt(res).array()[0];
+
+            buf[0] = (byte)res;//ByteBuffer.allocate(4).putInt(res).array()[0];
             writeLine(buf, 1);
         }
-        */
-        //buf[0]=1;
-        //writeLine("sss".getBytes(), 1);
-        System.out.write("ssss".getBytes());
     }
 }
